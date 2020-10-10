4de779d03807f8a378c8e4bb7306fc009997f94b
Alan <hy15816@qq.com>
2020-10-10 16:04:27 +0800

    first commit

---
+++ app/src/main/java/com/sxhl/heheda/PictureLoader.kt
@@ -0,0 +1,19 @@
+package com.sxhl.heheda
+
+import android.os.Bundle;
+import android.support.*;
+import android.widget.ImageView;
+import java.util.ArrayList
+import android.
+
+public class PictureLoader {
+    val imgv = ImageView(null);
+    val url = String();
+    val byteArr = ArrayList<String>();
+
+    Handle handle = new Handle() {
+        @Override
+        ha
+    }
+
+}
