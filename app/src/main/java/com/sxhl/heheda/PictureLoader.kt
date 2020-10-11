0fabc182ca2d0258b5923798c5d9e28980cc2a5a
Alan <hy15816@qq.com>
2020-10-11 10:09:40 +0800

    save

--- app/src/main/java/com/sxhl/heheda/PictureLoader.kt
+++ app/src/main/java/com/sxhl/heheda/PictureLoader.kt
@@ -4,16 +4,6 @@ import android.os.Bundle;
 import android.support.*;
 import android.widget.ImageView;
 import java.util.ArrayList
-import android.
 
 public class PictureLoader {
-    val imgv = ImageView(null);
-    val url = String();
-    val byteArr = ArrayList<String>();
-
-    Handle handle = new Handle() {
-        @Override
-        ha
-    }
-
 }
